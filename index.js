0d3912eec4d13d764e66910d3ab2c3fd12379cc9
Stefan Helms <mail@estefan.de>
2022-07-04 19:09:43 +0200

    toggle button js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ let button = document.querySelector('[data-js="button"]');
 
 function onButtonClick() {
   console.log("test");
-  alert("clicked");
-  button.classList.add("btn__test");
-  // button.classList.toggle("btn__test");
+  //alert("clicked");
+  //button.classList.add("btn__test");
+  button.classList.toggle("btn__test");
 }
 
 button.addEventListener("click", onButtonClick);
