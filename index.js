2ea8bdc10eac29e7a727395d06aa5d891bdf9022
Stefan Helms <mail@estefan.de>
2022-07-04 19:01:38 +0200

    Buttons in card fixed

---
+++ index.js
@@ -0,0 +1,10 @@
+let button = document.querySelector('[data-js="button"]');
+
+function onButtonClick() {
+  console.log("test");
+  alert("clicked");
+  button.classList.add("btn__test");
+  // button.classList.toggle("btn__test");
+}
+
+button.addEventListener("click", onButtonClick);
