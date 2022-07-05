07f553648bbc38f30d06b8ab44f1fc90fe6dde0e
Stefan Helms <mail@estefan.de>
2022-07-05 21:03:13 +0200

    Update create page with clean css

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 let button = document.querySelector('[data-js="button"]');
+let buttonBookmark = document.querySelector('[data-js="button__bookmark"]');
+let isBookmarked = false;
+
+//--//
+
+let answer = document.querySelector('[data-js="answer"]');
+
+answer.style.visibility = "hidden";
+
+//------------------------------------//
 
 function onButtonClick() {
-  console.log("test");
-  //alert("clicked");
-  //button.classList.add("btn__test");
-  button.classList.toggle("btn__test");
+
+  if (answer.style.visibility == "hidden") {
+    answer.style.visibility = "visible";
+    button.textContent  = "Hide answer";
+  } else {
+    answer.style.visibility = "hidden";
+    button.textContent  = "Show answer";
+  }
+
 }
 
 button.addEventListener("click", onButtonClick);
+
+function onBookmarkButtonClick() {
+  if (!isBookmarked) {
+    isBookmarked = true;
+    buttonBookmark.src = "/images/bookmark.svg";
+  } else {
+    isBookmarked = false;
+    buttonBookmark.src = "/images/bookmark-outline.svg";
+  }
+
+  //button.classList.add("btn__test");
+  //button.classList.toggle("btn__test");
+}
+
+buttonBookmark.addEventListener("click", onBookmarkButtonClick);
